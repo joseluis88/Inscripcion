0db8dea9c7710800febadbe122f46cb7126c7891
Alvaro Vazquez Gonzalez <practicasa1.slope@gmail.com>
2023-05-26 10:45:58 +0200

    importacion de icono en boton

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,8 +1,5 @@
 import "./App.css";
 
-// Iconos
-import CarbonTaskAdd from "./assets/CarbonTaskAdd";
-
 // Componentes
 import Inscribir from './Componentes/Botones/Inscribir'
 
@@ -10,8 +7,8 @@ function App() {
   return (
     <>
       <div className="App h-screen">
-        
-        <Inscribir titulo='Inscribir Jugador' Icono={CarbonTaskAdd}/>
+
+        <Inscribir titulo='Inscribir Jugador'/>
       </div>
     </>
   );
--- App/src/Componentes/Botones/Inscribir.jsx
+++ App/src/Componentes/Botones/Inscribir.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-const Inscribir = ({titulo, Icono}) => {
+// Iconos
+import CarbonTaskAdd from "./../../assets/CarbonTaskAdd";
+
+const Inscribir = ({titulo}) => {
   return (
     <div className="fixed bottom-0 w-full py-4 sm:py-8 flex justify-center">
       <button className="btn btn-wide w-1/2 justify-center place-items-center">
-        <Icono className="w-5 h-5 mx-2" />
+        <CarbonTaskAdd className="w-5 h-5 mx-2" />
         <span className="hidden sm:block">{titulo}</span>
       </button>
     </div>
